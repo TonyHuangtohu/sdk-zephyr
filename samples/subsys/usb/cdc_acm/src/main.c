74c84b216ce21f411b92becd8b6e86be8d38bdf9
Liuyong2 <tony.huang@nordicsemi.no>
2023-09-10 16:20:46 +0800

    Modified zephyr cdc_acm example to only enable usb serial port by shutting down uart0 and uart_console

--- samples/subsys/usb/cdc_acm/src/main.c
+++ samples/subsys/usb/cdc_acm/src/main.c
@@ -22,7 +22,7 @@
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/usb/usbd.h>
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(cdc_acm_echo, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(cdc_acm_echo, LOG_LEVEL_DBG);
 
 #define RING_BUF_SIZE 1024
 uint8_t ring_buffer[RING_BUF_SIZE];
